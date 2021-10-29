2f69c25dd11c22b6029bc138e3712aaddadf342c
Raúl Calvo Laorden <raulcalvolaorden@gmail.com>
2021-10-30 01:04:33 +0200

    Only acces login from each AP, untested

--- install/APs/html/index.php
+++ install/APs/html/index.php
@@ -11,11 +11,19 @@ echo "<br><br>";
 echo "<br><br>";
 
 if ($_SESSION["Username"]  == "GLOBAL\GlobalAdmin") {
-    echo "FLAG: WnIlomDsedsOj5I5AQzG0ZoMIhmJIJeejksyma64";
+    if (strpos($_SERVER['REMOTE_ADDR'], '192.168.7.') !== false) { //only TLS	
+		echo "FLAG: WnIlomDsedsOj5I5AQzG0ZoMIhmJIJeejksyma64";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
 }
 
 if ($_SESSION["Username"]  == "CONTOSO\Administrator") {
-    echo "FLAG: aY62tP67g5hK4f";
+	if (strpos($_SERVER['REMOTE_ADDR'], '192.168.5.') !== false) { //only TLS	
+		echo "FLAG: aY62tP67g5hK4f";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
 }
 
 if ($_SESSION["Username"]  == "admin") {
@@ -35,8 +43,63 @@ if ($_SESSION["Username"]  == "admin") {
 
 #ALL: and strpos($_SERVER['REMOTE_ADDR'], '192.168.X.') !== false to only use users in each network
 
+if ($_SESSION["Username"]  == "CONTOSO\juan.tr") {
+	if (strpos($_SERVER['REMOTE_ADDR'], '192.168.5.') !== false) { //only MGT	
+		echo "FLAG: TY62tPV7Vh1hK1o";
+		echo "<br><br>";
+		echo "Try default credentials";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
+}
+
+if ($_SESSION["Username"]  == "test1") {
+	if (strpos($_SERVER['REMOTE_ADDR'], '192.168.2.') !== false) { //only PSK	
+		echo "FLAG: uTJTGWAbpnxb6uO";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
+}
+
+if ($_SESSION["Username"]  == "test2") {
+	if (strpos($_SERVER['REMOTE_ADDR'], '192.168.2.') !== false) { //only PSK	
+		echo "FLAG: z39kTesr7hzgh9Y";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
+}
+
+if ($_SESSION["Username"]  == "free1") {
+    if (strpos($_SERVER['REMOTE_ADDR'], '192.168.0.') !== false) { //only OPN	
+		echo "FLAG: CNq6gDIqLfjROhN";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
+}
+
+if ($_SESSION["Username"]  == "free2") {
+	if (strpos($_SERVER['REMOTE_ADDR'], '192.168.0.') !== false) { //only OPN	
+		echo "FLAG: Bo8JJqByJ2b6yc4";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
+}
+
+if ($_SESSION["Username"]  == "anon1") {
+	# NO AP LOGIN
+    echo "FLAG: XhAK0YVSIwjeBDf";
+}
+
+if ($_SESSION["Username"]  == "administrator") {
+	if (strpos($_SERVER['REMOTE_ADDR'], '192.168.1.') !== false) { //only MGT	
+		echo "FLAG: Ke4kKmkrRc3dV0k";
+	} else {
+		echo "Your Princess Is in Another Castle!"
+	}
+}
+
 #relay user
-if ($_SESSION["Username"]  == "CONTOSOREG\luis.da") {
+if ($_SESSION["Username"]  == "CONTOSOREG\luis.da") { # RELAY
     echo "FLAG: F4G6$%2f2g4hs";
     echo "<br><br>";
     echo "<br><br>";
@@ -56,37 +119,6 @@ if ($_SESSION["Username"]  == "CONTOSOREG\luis.da") {
 		channel=1<br>";
 	    echo "Certificate Authority:  <a href=\"/secretCA/\"> http://", $_SERVER['SERVER_ADDR'], "/secretCA/ </a>";
 }
-
-if ($_SESSION["Username"]  == "CONTOSO\juan.tr") {
-    echo "FLAG: TY62tPV7Vh1hK1o";
-    echo "<br><br>";
-    echo "Try default credentials";
-}
-
-if ($_SESSION["Username"]  == "test1") {
-    echo "FLAG: uTJTGWAbpnxb6uO";
-}
-
-if ($_SESSION["Username"]  == "test2") {
-    echo "FLAG: z39kTesr7hzgh9Y";
-}
-
-if ($_SESSION["Username"]  == "free1") {
-    echo "FLAG: CNq6gDIqLfjROhN";
-}
-
-if ($_SESSION["Username"]  == "free2") {
-    echo "FLAG: Bo8JJqByJ2b6yc4";
-}
-
-if ($_SESSION["Username"]  == "anon1") {
-    echo "FLAG: XhAK0YVSIwjeBDf";
-}
-
-if ($_SESSION["Username"]  == "administrator") {
-    echo "FLAG: Ke4kKmkrRc3dV0k";
-}
-
 
 echo "<br><br>";
 echo "<br><br>";
